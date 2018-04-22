89b02bb58e32164a183c1544dc2a261224ce2857
Moritz Mair <mail@moritzmair.de>
2018-04-22 09:22:00 +0200

    added bootstrap model

--- app/assets/javascripts/bells.js
+++ app/assets/javascripts/bells.js
@@ -1,2 +1,18 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).ready(function(){
+
+  $(".modal").on('show.bs.modal', function () {
+    $('.blur').addClass('blur_active');
+  });
+
+  $(".modal").on('hide.bs.modal', function () {
+    $('.blur').removeClass('blur_active');
+  });
+
+  document.ontouchmove = function (e) {
+    e.preventDefault();
+  }
+  
+});
