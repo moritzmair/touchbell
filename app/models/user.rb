e6354db7916c566d23598a21326fd34cfe80a7b2
Moritz Mair <m.mair@iot-venture.com>
2019-07-31 10:28:12 +0200

    fixed problems in rails admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,24 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  extend Devise::Models
   has_many :bells
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable
 
+  # Provided for Rails Admin to allow the password to be reset
+  def set_password
+    nil
+  end
+
+  def set_password=(value)
+    return nil if value.blank?
+
+    self.password = value
+    self.password_confirmation = value
+    save
+  end
 end
