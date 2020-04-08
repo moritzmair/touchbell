5cdde402bcb9d4a16b9cb1c298d1d6469954f479
krishna <krishna.chaithanya.8@gmail.com>
2020-04-08 22:43:15 +0200

    check authHeader !null, using only request_body

--- app/controllers/bells_controller.rb
+++ app/controllers/bells_controller.rb
@@ -67,34 +67,39 @@ class BellsController < ApplicationController
     uri = URI.parse(@bell.trigger)
     # https://chat.inheaden.io/api/v4/posts
 
-    header = { "Content-Type": "application/json", "Authorization": "Token " + @bell.authorization_header }
-    #set auth_header in db, set trigger value should be /v4/posts
+    if (@bell.authorization_header != "NULL")
+      header = { "Content-Type": "application/json", "Authorization": "#{@bell.authorization_header}" }
+    else
+      header = { "Content-Type": "application/json" }
+    end
 
-    body = {
-      "channel_id" => "3aybp6hz5jfz3jjmyou39rtxth",
-      "message" => @bell.request_body,
-      "username" => "hub31_bell",
-    }
+    body = JSON.parse(@bell.request_body)
+    # body = { "channel_id" => "3aybp6hz5jfz3jjmyou39rtxth", "message" => "newest test", "username" => "hub31_bell" }
+
+    # body = {
+    #   "channel_id" => "3aybp6hz5jfz3jjmyou39rtxth",
+    #   "message" => @bell.request_body,
+    #   "username" => "hub31_bell",
+    # }
+
+    puts body
+    # {"channel_id"=>"3aybp6hz5jfz3jjmyou39rtxth", "message"=>"new test", "username"=>"hub31_bell"}
+
+    # 6hsw4y5pk7ym8csttnbbz9ecnh
+    # "3aybp6hz5jfz3jjmyou39rtxth", "test message", "hub31_bell"
+    # https://chat.inheaden.io/api/v4/posts
     # body = JSON.parse('{"channel_id": "3aybp6hz5jfz3jjmyou39rtxth", "message": @bell. ,"username":"hub31_bell"}')
-
-    # uri = URI.parse(@bell.trigger)
-
-    # header = { 'Content-Type': "application/json",
-    #            'Authorization': "Token " + @bell.authorization_header }
-    #set auth_header in db, set trigger value should be /v4/posts
-
-    # body = JSON.parse(@bell.request_body)
 
     # Create the HTTP objects
     http = Net::HTTP.new(uri.host, uri.port)
     request = Net::HTTP::Post.new(uri.request_uri, header)
     request.body = body.to_json
 
-    # puts user.to_json
-
-    # curl -a -H 'authorization: Token 6hsw4y5pk7ym8csttnbbz9ecnh' -H 'Content-Type: application/json' -d '{"channel_id": "3aybp6hz5jfz3jjmyou39rtxth", "message": "TEXT","username":"hub31_bell"}' https://chat.inheaden.io/api/v4/posts
-
+    #set auth_header in db, set trigger value should be /v4/posts
     #generate migration, add coloumun, db migrate,
+    # curl -a -H 'authorization: Token 6hsw4y5pk7ym8csttnbbz9ecnh' -H 'Content-Type: application/json' -d '{"channel_id": "3aybp6hz5jfz3jjmyou39rtxth", "message": "TEXT","username":"hub31_bell"}' https://chat.inheaden.io/api/v4/posts
+    # set auth_header in db, set trigger value should be /v4/posts
+
     # Send the request
     res = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == "https") do |http|
       http.request(request)
@@ -117,18 +122,14 @@ class BellsController < ApplicationController
     render json: response.to_json
   end
 
-  # if @bell.request_type == 'GET'
-
-  # end
-
-  # def select_method_type
-  # bell_id_from_UI = @bell.id
-  # puts bell_id_from_UI
   def ringring
-    if @bell.request_type == "POST"
+    case @bell.request_type
+    when "POST"
       post_service
-    else
+    when "GET"
       get_service
+    else
+      puts Unknown Request Type
     end
   end
 
