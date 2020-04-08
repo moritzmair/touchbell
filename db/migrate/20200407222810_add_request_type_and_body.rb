1841cce56e9dc11b7a9d109f47d3a1ce85d79920
krishna <krishna.chaithanya.8@gmail.com>
2020-04-08 10:44:52 +0200

    delete & add new migrate file

---
+++ db/migrate/20200407222810_add_request_type_and_body.rb
@@ -0,0 +1,7 @@
+class AddRequestTypeAndBody < ActiveRecord::Migration[5.1]
+  def change
+    add_column :bells, :request_type, :string, default: "GET"
+    add_column :bells, :request_body, :string
+    add_column :bells, :authorization_header, :string
+  end
+end
