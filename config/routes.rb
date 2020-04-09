354b298b9fafcc008000fbaaa8e3307aa54f5e7f
Lars Gröber <lars.groeber@inheaden.io>
2020-04-09 11:30:40 +0200

    small clean up

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,3 @@ Rails.application.routes.draw do
   get '/ringring/:id' => 'bells#ringring'
   root to: 'welcome#home'
 end
-
-#create new route, create uri(unique route identification)
-# write in words --> whatever they have/create bell/new bell
-# create uri then call it from UI
-#data structure also - not so and in UI -> when that matches it can call --> call in postman
-
-
