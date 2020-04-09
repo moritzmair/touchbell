ff028fa3a5b91264d27651cb0e2f851df27e9c54
Lars Gröber <lars.groeber@inheaden.io>
2020-04-09 11:24:45 +0200

    small clean up

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200407181316) do
+ActiveRecord::Schema.define(version: 20200407222810) do
   create_table "bells", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
     t.string "trigger"
